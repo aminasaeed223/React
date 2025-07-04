f4fd67fd4fbfa68a3b2719bad31664422a51019b
Amina Saeed <aminasaeed223@gmail.com>
2025-07-04 12:18:32 +0500

    commit

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,13 +1,18 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import React from 'react';
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [val, setVal] = useState("Amina Saeed")
 
   return (
     <>
-      <h1 className='text-4xl text-center text-orange'>Password Generator</h1>
+     <h1>Get Input field</h1>
+     <input type="text" value= {val} onChange={(event)=>setVal(event.target.value)} placeholder = "Enter your text"/>
+      <h1>{val}</h1>
+      <button onClick={()=>{setVal("")}}>Clear
+      
+      </button>
     </>
   )
 }
