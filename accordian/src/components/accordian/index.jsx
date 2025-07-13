ce4a723e338446d334186faf856d1b68b7d1ab4e
Amina Saeed <aminasaeed223@gmail.com>
2025-07-13 15:50:01 +0500

    commit

--- accordian/src/components/accordian/index.jsx
+++ accordian/src/components/accordian/index.jsx
@@ -1,17 +1,28 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { useRef } from "react";
 
 function Accordian(){
-    const [selected, setSelected] = useState(null);
+    // const [selected, setSelected] = useState(null);
+    const inputRef = useRef(null);
+    function handle (){
+        console.log(inputRef);
+        inputRef.current.focus();
+    }
     return(
         <>
-        <div className="wrapper">
+        <h1>Useref</h1>
+        <input ref = {inputRef} type="text" placeholder="enter your name" />
+        <button onClick={handle}>Submit</button>
+{/* 
+
+        { <div className="wrapper">
             <div className="accordian">
                 {
                     data && data.length >0 ? : <p>Data is not found</p>
                 }
             </div>
 
-        </div>
+        </div> } */}
         
         </>
     )
